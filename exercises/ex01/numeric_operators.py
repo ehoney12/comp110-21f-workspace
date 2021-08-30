eb99e4827d10560351524d624ce9547057bc7474
Elizabeth Park <ehoney@live.unc.edu>
2021-08-30 19:42:44 -0400

    Last changes to exercises 1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -4,4 +4,11 @@ __author__ = "730240245"
 
 int1: int = int(input("Left-hand side: "))
 int2: int = int(input("Right-hand side: "))
-times: float(int1 ** int2)
+times: int = (int1 ** int2)
+division: float = (int1 / int2)
+integer_division: int = (int1 // int2)
+remainder: int = (int1 % int2)
+print(str(int1) + " **" + str(int2) + " is " + str(times))
+print(str(int1) + " / " + str(int2) + " is " + str(division))
+print(str(int1) + " // " + str(int2) + " is " + str(integer_division))
+print(str(int1) + " % " + str(int2) + " is " + str(remainder))
