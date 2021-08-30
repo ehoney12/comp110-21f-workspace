b7ddb9da2555aef6ac7a5f497584d3d6291936fd
Elizabeth Park <ehoney@live.unc.edu>
2021-08-30 19:03:49 -0400

    Changes to exercise 1

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -4,7 +4,9 @@ __author__ = "730240245"
 
 int1 = input("Left-hand side: ")
 int2 = input("Right-hand side: ")
-print(int1 + " < " + int2 + " is " + str(int1 < int2))
-print(int1 + " >= " + int2 + " is " + str(int1 >= int2))
+less: bool = (int1 < int2)
+greater_or_equal: bool = (int1 >= int2)
+print(int1 + " < " + int2 + " is " + str(less))
+print(int1 + " >= " + int2 + " is " + str(greater_or_equal))
 print(int1 + " == " + int2 + " is " + str(int1 == int2))
 print(int1 + " != " + int2 + " is " + str(int1 != int2))
