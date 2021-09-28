7040e9375f4f0bfa529c6da16c81f5c4e2d91748
Elizabeth Park <ehoney@live.unc.edu>
2021-09-27 21:25:17 -0400

    exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,40 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730240245"
 
 
 # TODO: Implement your functions here.
+def all(y: list[int], x: int) -> bool: 
+    """Return True if list numbers match x, Return False otherwise or an Empty List."""
+    i: int  = 0 
+    while i < len(y):
+        number: int = y[i]
+        if x != number or y == list(): 
+            return False 
+        i = i + 1 
+    return True 
+
+
+def is_equal(a: list[int], b: list[int]) -> bool: 
+    """Return True if lists are equal, return false otherwise."""
+    i: int = 0 
+    while i < len(a): 
+        list_a: int = a[i]
+        list_b: int = b[i]
+        if len(a) != len(b) or list_a != list_b:
+            return False 
+        i = i + 1 
+    return True 
+
+
+def max(input: list[int]) -> int: 
+    """Return the largest int value in the list."""
+    if input == list():    
+        raise ValueError("max() arg is an empty List")
+    i: int = 0 
+    biggest: int = -99999 
+    while i < len(input):
+        if input[i] > biggest: 
+           biggest = input[i] 
+        i = i + 1 
+    return biggest
