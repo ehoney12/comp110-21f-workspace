89a93841e340b9bd71031c405ad6168bc28f3cc8
Elizabeth Park <ehoney@live.unc.edu>
2021-10-18 17:24:13 -0400

    saved changes to ex06 pt 2

--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -18,43 +18,43 @@ def invert_letters() -> None:
     assert invert(input) == {'r': 'c', 'q': 't', 'w': 'b'}
 
 
-def invert_words() -> None: 
+def test_invert_words() -> None: 
     """Invert function use case testing with words."""
     input: dict[str, str] = {'hi': 'hey', 'who': 'you'}
     assert invert(input) == {'hey': 'hi', 'you': 'who'} 
 
 
-def favorite_color_tie_between_two() -> None:
+def test_favorite_color_tie_between_two() -> None:
     """Favorite_color function use case testing with a tie between two colors."""
     x: dict[str, str] = {'Ally': 'blue', 'Karen': 'purple', 'Jeff': 'blue', 'Carol': 'purple'}
     assert favorite_color(x) == 'blue'
 
 
-def favorite_color_all_different_colors() -> None:
+def test_favorite_color_all_different_colors() -> None:
     """Favorite_color function use case testing with all different colors."""
     x: dict[str, str] = {'Steven': 'green', 'Mark': 'grey', 'Tony': 'orange'}
     assert favorite_color(x) == 'green'
 
 
-def favorite_color_one_color() -> None:
+def test_favorite_color_one_color() -> None:
     """Favorite_color function edge case testing with one color."""
     x: dict[str, str] = {'Mary': 'pink', 'Sara': 'pink', 'Anthony': 'pink'}
     assert favorite_color(x) == 'pink'
 
 
-def count_empty_list() -> None:
+def test_count_empty_list() -> None:
     """Count functon edge case testing with empty list."""
     c: list[str] = []
     assert count(c) == {}
 
 
-def count_different_str() -> None:
+def test_count_different_str() -> None:
     """Count function use case testing with different words."""
     c: list[str] = ['fall', 'pumpkins', 'Halloween', 'candy']
     assert count(c) == {'fall': 1, 'pumpkins': 1, 'Halloween': 1, 'candy': 1}
 
 
-def count_duplicates_and_more() -> None: 
+def test_count_duplicates_and_more() -> None: 
     """Count function use case testing with words that appear multiple times in list."""
     c: list[str] = ['coffee', 'sugar', 'spice', 'sugar', 'sugar', 'coffee', 'cream']
     assert count(c) == {'cofee': 2, 'sugar': 3, 'spice': 1}
